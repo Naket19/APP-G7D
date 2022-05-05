efc79cf5f4b0e957a304bdaffd7b2b7987cc17db
elliotbertrand <62110738+elliotbertrand@users.noreply.github.com>
2022-05-06 00:13:38 +0200

    Ajustement lien menu

--- connexion.php
+++ connexion.php
@@ -72,7 +72,7 @@ if(isset($_POST['connexion'])){
                 <i class="fa-solid fa-circle-xmark fermer"></i>
             </div>
             <div class="acc-menu">
-                <a href="Index.html">Accueil</a>
+                <a href="Index.php">Accueil</a>
                 <a href="">Données Capteurs</a>
                 <a href="P_faq.html">FAQ</a>
                 <a href="">Application Ludique</a>
