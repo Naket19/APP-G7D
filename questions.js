eff1a051fd50f5f4ba6ab5a06db484291df36887
aymenisep <aymen2001@hotmail.fr>
2022-05-26 22:07:39 +0200

    appli lud

---
+++ questions.js
@@ -0,0 +1,351 @@
+let questions = [
+    {
+      numb: 1,
+      question: "Quelle est la taille moyenne d'un nouveau né ?",
+      answer: "Il mesure entre 45 et 55 cm",
+      options: [
+        "Il mesure entre 45 et 55 cm",
+        "Il mesure entre 35 et 45 cm",
+        "Il mesure entre 25 et 35 cm",
+        "Il mesure entre 55 et 65 cm"
+      ]
+  },
+    {
+      numb: 2,
+      question: "Quelle est le poids moyen d'un nouveau né ?",
+      answer: "Il pèse entre 2500 et 4300 g",
+      options: [
+        "Il pèse entre 2500 et 4300 g",
+        "Il pèse entre 1500 et 3300 g",
+        "Il pèse entre 3500 et 5300 g",
+        "Il pèse entre 2500 et 3300 g"
+      ]
+  },
+    {
+      numb: 3,
+      question: "Pourquoi les yeux des nouveaux nés sont bleus ?",
+      answer: "La production de mélanine après la naissance",
+      options: [
+        "La production de mélanine après la naissance",
+        "Les bébés n’ont pas de yeux bleus a la naissance",
+        "Seuls les bébés ayant les yeux bleus les ont a la naissance",
+        "Parce que ils manquent de sucre"
+      ]
+  },
+    {
+      numb: 4,
+      question: "Combien d’os possède un bébé ?",
+      answer: "Environ 300",
+      options: [
+        "Environ 300",
+        "Environ 400",
+        "Environ 500",
+        "Environ 200"
+      ]
+  },
+    {
+      numb: 5,
+      question: "Quel vaccin est obligatoire après 2 mois? ",
+      answer: "Gastro à rotavirus ",
+      options: [
+        "Gastro à rotavirus ",
+        "VIH",
+        "Tetanos",
+        "Covid-19"
+      ]
+  },
+    {
+    numb: 6,
+    question: "Quel vaccin est obligatoire après 12 mois?",
+    answer: "Rougeole",
+    options: [
+      "Rougeole ",
+      "Méningites",
+      "Hépatite B",
+      "Pneumonie"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Les avantages de l'allaitement au biberon? ",
+    answer: "Le biberon permet de connaitre la quantité exacte de lait bu",
+    options: [
+      "Le biberon apporte plus de fibre ",
+      "Le biberon est plus facile a digérer",
+      "Le biberon permet de connaitre la quantité exacte de lait bu",
+      "Le biberon permet une meilleure formation dentaire"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Les avantages de l’allaitement au sein? ",
+    answer: "Le lait maternel offre la teneur parfaite en protéines, en glucides, en matières grasses, en vitamines et en minéraux et est facile à digérer.",
+    options: [
+      "Le lait maternel offre la teneur parfaite en fibre et en calories. ",
+      "Le lait maternel diminue le risque de cancer ",
+      "Le lait maternelle permet de créer un lien enfant/parent",
+      "Le lait maternel offre la teneur parfaite en protéines, en glucides, en matières grasses, en vitamines et en minéraux et est facile à digérer."
+    ]
+  },
+  {
+    numb: 9,
+    question: "De quel couleur voit un bébé a la naissance ?",
+    answer: "Noir et blanc ",
+    options: [
+      "De toutes les couleurs ",
+      "Noir et blanc ",
+      "Toutes les couleurs sauf le bleu",
+      "Il ne voit pas "
+    ]
+  },
+  {
+    numb: 10,
+    question: "Quel est le facteur de multiplication moyen du poids d’un bébé au bout d’un an ?",
+    answer: "2x",
+    options: [
+      "5x ",
+      "4x",
+      "3x",
+      "2x"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Combien de temps faut-il pour qu’un bébé s’adapte à la vie en dehors de l’utérus ?",
+    answer: "3 mois",
+    options: [
+      "3 semaines ",
+      "4 semaines ",
+      "3 mois",
+      "4 mois"
+    ]
+  },
+  {
+    numb: 12,
+    question: "Quel est l’odeur d’un bébé a la naissance ?",
+    answer: "Il n’a pas d’odeur",
+    options: [
+      "L’odeur de l’utérus  ",
+      "Odeur fruitée ",
+      "Dépend des aliments consommés par la mère",
+      "Il n’a pas d’odeur"
+    ]
+  },
+  {
+    numb: 13,
+    question: "Combien de dents possède un bébé a sa naissance?",
+    answer: "Il peut en avoir une mais c’est rare",
+    options: [
+      "Il a toutes ses dents",
+      "Il peut en avoir une mais c’est rare",
+      "Il possède juste ses gencives ",
+      "Il n’en possède pas "
+    ]
+  },
+  {
+    numb: 14,
+    question: "Quelle échographie permet de déterminer de façon certaine le sexe du bébé ?",
+    answer: "2 ème échographie",
+    options: [
+      "1 ère échographie",
+      "2 ème échographie ",
+      "3 ème échographie",
+      "4 ème échographie"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Le bébé peut-il avoir le hoquet ?",
+    answer: "Oui ",
+    options: [
+      "Non  ",
+      "Oui  ",
+      "Dans des cas extrêmement rare et c’est dangereux ",
+      "Un bébé a toujours un hoquet à la naissance"
+    ]
+  },
+  {
+    numb: 16,
+    question: "Le fœtus dort-il environ ?",
+    answer: "20 h",
+    options: [
+      "5 h ",
+      "10 h ",
+      "20 h",
+      "25 h"
+    ]
+  },
+  {
+    numb: 17,
+    question: "Le coeur de l’embryon bat :",
+    answer: "Apres 3 semaines de grossesse",
+    options: [
+      "Apres 3 semaines de grossesse ",
+      "Apres 3 jours après la conception ",
+      "Apres 9 semaines de grossesse",
+      "Apres 6 semaines de grossesse "
+    ]
+  },
+  {
+    numb: 18,
+    question: "Les battements du cœur fœtal sont environ :",
+    answer: "2 fois plus rapides que les nôtres",
+    options: [
+      "2 fois plus rapides que les nôtres ",
+      "A la même mesure que les nôtres",
+      "2 fois plus lents que les nôtres",
+      "1.5 fois plus rapides que les nôtres"
+    ]
+  },
+  {
+    numb: 19,
+    question: "Quel sens apparaît en premier chez le futur bébé ?",
+    answer: "Le toucher ",
+    options: [
+      "Le gout ",
+      "Le toucher ",
+      "L’ouïe ",
+      "la proprioception"
+    ]
+  },
+  {
+    numb: 20,
+    question: "Le sexe du bébé est déterminé dès la fécondation ?",
+    answer: "Vrai",
+    options: [
+      "Vrai ",
+      "Faux",
+      "Ca dépend des cas",
+      "La réponse D"
+    ]
+  },
+  {
+    numb: 21,
+    question: "A la fin du sixième mois de grossesse, le fœtus pèse :",
+    answer: "Environ 900 g",
+    options: [
+      "Environ 2 kg",
+      "Environ 1,5 kg ",
+      "Environ 1,2 kg",
+      "Environ 900 g"
+    ]
+  },
+  {
+    numb: 22,
+    question: "Le réflexe de succion apparaît chez le fœtus :",
+    answer: "Au deuxième trimestre de la grossesse",
+    options: [
+      "Au premier trimestre de la grossesse  ",
+      "Au deuxième trimestre de la grossesse",
+      "Au troisième trimestre de la grossesse",
+      "Au quatrième trimestre de la grossesse"
+    ]
+  },
+  {
+    numb: 23,
+    question: "Les paupières du fœtus commencent à s’ouvrir :",
+    answer: "Au 5ème mois de grossesse",
+    options: [
+      "A la naissance ",
+      "Au 3 ème mois de grossesse ",
+      "Au 5ème mois de grossesse",
+      "Au 9 ème mois de grossesse"
+    ]
+  },
+  {
+    numb: 24,
+    question: "Chez les faux jumeaux :",
+    answer: "Deux spermatozoides fécondent deux ovules",
+    options: [
+      "Deux spermatozoides fécondent 1 ovule ",
+      "Un spermatozoide féconde deux ovules  ",
+      "Un spermatozoide féconde un ovule, qui se partage en deux ",
+      "Deux spermatozoides fécondent deux ovules"
+    ]
+  },{
+    numb: 25,
+    question: "L’embryon se transforme en fœtus :",
+    answer: "Au troisième mois de grossesse",
+    options: [
+      "Au premier mois de grossesse ",
+      "Au troisième mois de grossesse",
+      "Au quatrième mois de grossesse ",
+      "Au cinquième mois de grossesse"
+    ]
+  },{
+    numb: 26,
+    question: "Enceinte, il est conseillé de boire au minimum :",
+    answer: "2 litres d’eau par jour",
+    options: [
+      "1 litre d’eau par jour",
+      "1,5 litre d’eau par jour  ",
+      "2 litres d’eau par jour",
+      "2,5 litres d’eau par jour"
+    ]
+  },{
+    numb: 27,
+    question: "Combien de repas un nourrisson prend-il chaque jour ? ",
+    answer: "8 repas par jour",
+    options: [
+      "5 repas par jour ",
+      "6 repas par jour",
+      "7 repas par jour",
+      "8 repas par jour"
+    ]
+  },
+  {
+    numb: 28,
+    question: "Combien de temps votre partenaire et vous-même devez attendre après l’accouchement avant d’avoir des relations sexuelles ?",
+    answer: "Environ 6 semaines",
+    options: [
+      "Environ 4 semaines",
+      "Environ 5 semaines",
+      "Environ 6 semaines",
+      "Environ 7 semaines"
+    ]
+  },
+  {
+    numb: 29,
+    question: "A combien de décibels un bébé peut-il crier ?",
+    answer: "Jusqu’à 120 décibels  ",
+    options: [
+      "Jusqu’à 120 décibels   ",
+      "Jusqu’à 140 décibels  ",
+      "Jusqu’à 160 décibels  ",
+      "Jusqu’à 180 décibels  "
+    ]
+  },
+  {
+    numb: 30,
+    question: "De quoi avez-vous besoin pour nettoyer le nombril de ton bébé ? ",
+    answer: "De l’eau tiède",
+    options: [
+      "De l’eau tiède",
+      "un Produiant de pérénisation",
+      "Un coton-tige ",
+      "Du désinfectant "
+    ]
+  },
+  {
+    numb: 31,
+    question: "A quel âge les bébés commencent-ils à faire leurs nuits ?",
+    answer: "Environ 3 mois  ",
+    options: [
+      "Environ 3 mois  ",
+      "Environ 6 mois  ",
+      "Environ 9 mois  ",
+      "Environ 1 ans  "
+    ]
+  },
+  {
+    numb: 32,
+    question: "Quelle est la position recommandée pour coucher le bébé ? ",
+    answer: "Sur le dos",
+    options: [
+      "Sur le ventre",
+      "Sur le dos",
+      "Assis",
+      "En PLS (position latéral de sécurité)"
+    ]
+  },
+];
