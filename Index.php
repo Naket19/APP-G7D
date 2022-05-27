f7c650837605b5c10fa0fab04c367053c018c638
jackson-mb <68854018+jackson-mb@users.noreply.github.com>
2022-05-27 10:21:25 +0200

    Page d'accueil terminée. Responsive opérationnel, pas de scroll.

--- Index.php
+++ Index.php
@@ -9,13 +9,14 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" 
     integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" 
     crossorigin="anonymous" referrerpolicy="no-referrer" /> 
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
     <link rel="stylesheet" href="CSS/responsive.css">
     <title>Infinite Measure</title>
 </head>
 <body>
     <header>
-        
+
         <nav>
             <img src="image/logo_infinte_measure.png" alt="">
             <div class="toggle">
@@ -37,91 +38,140 @@
             ><a href="connexion.php" style='text-decoration:none;color:black;'> Connexion</a></button>
         </nav>
     </header>
-    <div class="contenu">
-        <div class="landing-page">
-            <h1 class="big-title">Bienvenue sur notre site médical !
-            </h1>
-            <h1 class="accueil-text"> Accueil <br><br>In sententia agendae rei
-                ut quadrigis, ad applicationem enucleare ducimur
-                 textus. Ad hoc primum propositum est definiendum, i.e.
-                 functiones quam applicationes nostras debebunt providere, habita ratione
-                 fatalibus nobis datis. Tunc consilium eligemus faciendo
-                 ex intentione applicationis nostrae ac denique technologiae interretialem eligendi habebimus
-                 uti ad nostram applicationem aedificandam. In sequentibus, opus nostrum explicandum erit
-                 delectu technologiarum interretialium necnon applicationis architecturae telae huius incepti.</h1> 
+    <div class="test-contenu">
+        <div class="barre-vertical">
+            <div class="elements-pages">
+                <li class = product
+                onclick="document.getElementById('part1').style.display='block';
+                document.getElementById('gauche').style.display='none';
+                document.getElementById('presentation').style.display='block';
+                document.getElementById('avantages').style.display='none'"
+                >
+                Notre produit
+                </li>
+                <li class="produit" 
+                onclick="document.getElementById('part1').style.display='block';
+                document.getElementById('gauche').style.display='block';
+                document.getElementById('avantages').style.display='none'">Notre Produit</li>
+
+                <li class="why"onclick="document.getElementById('part1').style.display='none';
+                document.getElementById('avantages').style.display='block'"
+                >Pourquoi notre produit ?</li>
+
+                <li class="fonct" onclick="document.getElementById('part1').style.display='block';
+                document.getElementById('gauche').style.display='block';
+                document.getElementById('avantages').style.display='none';
+                document.getElementById('presentation').style.display='none';"
+                > <a href=""></a> fonctionnement </li>
+            </div>
         </div>
-        <div class="phase">
-            <H1> Infinite Measure pour vous et votre bébé  !</H1>
+        <div class="scroll">
+            <div id= "part1" class="Part1 ">
+                <div class="img-medecin">
+                    <div class="gauche" id="gauche">
+                        <img src="image/medecins-soignants.jpg" alt="">
+                        <div class="fonctionnement">
+                            <h3>Fonctionnement du produit</h3>
+                            <p> Nos capteurs sont mis sur vos enfants 
+                                ensuite les données sont récoltées et envoyer
+                                sur le site web où le corps médical peut
+                                vérifier que les constantes de vos enfants et 
+                                de l'environnement où il se trouve sont 
+                                bonnes.  
+                            </p>
+                        </div>
+                    </div>
+
+                    <div id="presentation" class="presentation">
+                        <h3>Presentation du produit  </h3>
+                        <p>c'est un dispositif électronique constitué de 
+                            4 capteurs permettant d'obtenir les données de 
+                            votre enfant et son environnement.
+                        </p>
+                        <div class="text1">
+                            <div class="cardiaque"style="display: flex;
+                            align-items:center;flex-direction:row">
+                                <i style="display: flex; font-size: 25px;
+                                padding:8px;border-radius:70px; width:25px;
+                                align-items: center;
+                                background: url(../image/back-ecriture.png);"
+                                class="uil uil-heartbeat"></i>
+                                <p> un capteur cardiaque pour pouvoir vérifier
+                                    la fréquence cardiaque.
+                                </p>
+                            </div>
+                            <div class="sonnore" style="display: flex;
+                                align-items:center;flex-direction:row">
+                                <i style="display: flex; font-size: 25px;
+                                    padding:8px;border-radius:70px; height:25px;
+                                    align-items: center;
+                                    background: url(../image/back-ecriture.png);"
+                                    class="uil uil-volume-up">
+                                </i>
+                                <p style="display: flex;"> 
+                                    Un capteur Sonnore pour mesurer le niveau 
+                                    de bruit autour de votre enfant. 
+                                </p>
+                            </div>
+                            <div class=" humidité" style="display: flex;
+                            align-items:center;flex-direction:row">
+                                <i style="display: flex; font-size: 25px;
+                                padding:8px; border-radius:70px; height:25px;
+                                align-items: center;
+                                background: url(../image/back-ecriture.png);"
+                                class="uil uil-tear"></i>
+                                <p style="display: flex;">
+                                    Un capteur d'humidité pour mesurer le niveau 
+                                    de d'humidité et la temprérature
+                                    autour de votre enfant. 
+                                </p>
+                            </div>
+                            <div class="CO2" style="display: flex;
+                                align-items:center;flex-direction:row">
+                                <i style="display: flex; font-size: 25px;
+                                padding:8px; border-radius:70px; height:25px;
+                                align-items: center;
+                                background: url(../image/back-ecriture.png);"
+                                class="uil uil-comparison"></i>
+                                <p> Un capteur de C02 pour mesurer le niveau 
+                                    de CO2 dans l'air autour de votre enfant. 
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="part2">
+                <div id= "avantages"class="avantages">
+                    <h2>Les avantages de notre produit</h2>
+                    <div class="Apport">
+                        <h3>Notre apport</h3>
+                        <p>Nous fournissons une vision globale sur 
+                            la prise en charge d’un nouveau-né.
+                             Que vous soyez médecin,
+                             parent ou membre de la famille,
+                             vous aurez un suivi détaillé et
+                              intuitif du bien-être du bébé. 
+                        </p>
+                    </div>
+                    <div class="data">
+                        <h3> Vos données</h3>
+                        <p>Notre produit récolte différentes types de 
+                            données (sonore, cardiaque, qualité de l’air)
+                            afin d’assurer la bonne santé du nourrisson.
+                        </p>
+                    </div>
+                    <div class="a-faire">
+                        <h3> Une seule chose à faire</h3>
+                        <p>Créez votre compte et accédez à l'affichage
+                            des résultats du dispostif électronique sur
+                            notre site internet ergonomique.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-    <div class="presentation">
-        <img src="image/femme sur son PC.webp" alt="">
-        <div class="avantage">
-            <p style="text-align: justify"><li>Surveiller votre bébé
-            <br> Monitor vestri infantem scriptor
-             parametri diversis etiam procul</li>
-        </p>
-        <p style="text-align: justify"> <li> Communiquer avec le corps médical
-            <br>Communicare cum medicinae professione
-            etiam remotius ad omnium commodum</li>
-        </p>
-        <p style="text-align: justify"> <li> Un système d'alerte en temps réel
-            <br>Optimize vestri infantem scriptor maneat
-            sana cum relinquere infantem</li>
-        </p>
-    </div>
-    
-       
-    </div>
-    </div>
-    <div class="titreproduit">
-        <H1> NOTRE PRODUIT</H1>
-    
-
-    <div class="produit">
-        <p style="text-align: justify"><li>UN BOÎTIER
-            <br> Composé de plusieurs capteurs de <br> tous  types afin de 
-            s'assurer du bien-être de votre enfant à tous les niveaux</li>
-        </p>
-        <p style="text-align: justify"><li>UNE PLATEFORME
-            <br> Permetant d'acceder aux données de vos enfants à chaque instant </li>
-        </p>
-        <p style="text-align: justify"><li>UN LIEN
-            <br> Permettant de rapprocher les parents des enfants</li>
-        </p>
-
-    </div>
-    <div class="imgmaternité">
-        <img src="image/maternité.jpg" alt="" width="900" height="500" style="border-radius: 14px">
-
-    </div>
-    </div>
-    <div class="titreutilisateurs">
-        <h1 class="titreutil">NOS UTILISATEURS </h1> <br>
-        <div class="utilisateurs">
-        <p> <h1>75% <br> HOPITAUX</h1>
-          
-        </p>
-        <p><h1>20% <br> CLINIQUES</h1>
-             
-        </p>
-        <p><h1>5% <br> PARTICULIERS</h1>
-            
-        </p>
-    </div></div>
-
-    
-    <div class="histoire">
-        <h1>NOTRE HISTOIRE</h1> <br>
-        <p style="text-align: justify; margin: auto 40px">L'entreprise Infinite Measures est née d'une association de plusieurs entrepreneurs
-        à succès partageant le même objectif d'amélioration et de sécurisation de tâches du quotidien
-        et quoi de mieux que de protéger les générations futures.</p><br>
-
-
-    </div>
-
-
-    
-    
+    </div>       
     <footer>
         <a href="P_nousContacter.php">Nous contacter</a>
         <a href="mentions_légales.html">Mentions légales</a>
